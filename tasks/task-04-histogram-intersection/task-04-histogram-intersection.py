98f68b949287ff3af0c3ce5a9cd864c787190600
Gustavo Gaia <gu.gaia16@outlook.com>
2025-08-10 21:20:15 -0300

    Task 4 - Histogram Intersection task concluded

--- tasks/task-04-histogram-intersection/task-04-histogram-intersection.py
+++ tasks/task-04-histogram-intersection/task-04-histogram-intersection.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 def compute_histogram_intersection(img1: np.ndarray, img2: np.ndarray) -> float:
     """
@@ -25,10 +26,13 @@ def compute_histogram_intersection(img1: np.ndarray, img2: np.ndarray) -> float:
     if img1.ndim != 2 or img2.ndim != 2:
         raise ValueError("Both input images must be 2D grayscale arrays.")
 
-    ### START CODE HERE ###
-    # Step 1: initialize base image with 0.5
-    intersection = 0.0
-    ### END CODE HERE ###
+    histogramImg1, _ = np.histogram(img1, bins=256, range=(0, 255))
+    histogramImg2, _ = np.histogram(img2, bins=256, range=(0, 255))
 
+    #Normalizando valores
+    histogramImg1 = histogramImg1.astype(float)/ histogramImg1.sum()
+    histogramImg2 = histogramImg2.astype(float)/ histogramImg2.sum()
+
+    intersection = np.sum(np.minimum(histogramImg1, histogramImg2))
 
     return float(intersection)
